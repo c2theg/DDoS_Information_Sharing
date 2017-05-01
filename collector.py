5a6bcf9ab83564739419dcb0f5645dc4e777078f
Chris Gray <christophermjgray@gmail.com>
2017-05-01 00:50:11 -0400

    Updates

--- collector.py
+++ collector.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
 # Copyright (c) 2016-2017 Christopher Gray & Daniel Phan - Comcast Cable Communications LLC.
+# Copyright (c) 2017 Rich Compton - Charter Communications
 # All rights reserved.
 # https://github.com/c2theg/DDoS_Infomation_Sharing
 
@@ -11,20 +13,23 @@ import argparse
 from datetime import datetime
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 appTime = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-appVersion = '0.1.12'
+appVersion = '0.1.15'
 appCurrentPath = os.getcwd()
 appCurrentPath.replace("\/",'\\/')
 
 appHeader = """\
-     _____ _       _     _           _              _____    __    _____             
-    |     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
-    |   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
-    |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
-                                |_|                                             |___|
-                                      __     
-                                     / _|___ 
-                                     > _|_ _|
-                                     \_____| 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+                                           _   
+                                         _| |_ 
+                                        |   __|
+                                        |   __|
+                                        |_   _|
+                                          |_|                              
                                              
                       ___            _     _   ___ _              
                      |   \ __ _ _ _ (_)___| | | _ \ |_  __ _ _ _  
@@ -39,7 +44,20 @@ appHeader = """\
                       / __|___ _ __  _ __     | |  | |  / __|
                      | (__/ _ \ '  \| '  \ _  | |__| |_| (__ 
                       \___\___/_|_|_|_|_|_(_) |____|____\___|
-                                                             
+                                     
+-----------------------------------------------------------------------------------------
+                                                                  
+             _____ _     _      _____               _                         
+            | __  |_|___| |_   |     |___ _____ ___| |_ ___ ___               
+            |    -| |  _|   |  |   --| . |     | . |  _| . |   |              
+            |__|__|_|___|_|_|  |_____|___|_|_|_|  _|_| |___|_|_|              
+                                               |_|                            
+                                                                              
+                     _____ _           _              _____                   
+                    |     | |_ ___ ___| |_ ___ ___   |     |___ _____ _____   
+                    |   --|   | .'|  _|  _| -_|  _|  |   --| . |     |     |_ 
+                    |_____|_|_|__,|_| |_| |___|_|    |_____|___|_|_|_|_|_|_|_|
+                                                                                                       
                           ___ __  _  __         ___ __  _ ____ 
                          |_  )  \/ |/ /   ___  |_  )  \/ |__  |
                           / / () | / _ \ |___|  / / () | | / / 
@@ -49,12 +67,34 @@ appHeader = """\
 print appHeader
 print "                  DDoS Source Information Sharing - Syslog Collector"
 print "                                    Version ", appVersion
-print "            Get Updates from: https://github.com/c2theg/DDoS_Infomation_Sharing \r\n\r\n\r\n"
+print "            Get Updates from: https://github.com/c2theg/DDoS_Infomation_Sharing \r\n\r\n"
+
+appInfo = """
+ 
+This script listens on a port (defined in the config.json file) for syslog messages from Arbor Peakflow and when it sees a message that a DDoS attack has stopped, 
+it will launch getSource.py script which query Peakflow\'s API and retrieve the source IPs associated with the attack and then upload them to the defined CRIT\'s database.
+"""
+print appInfo
+print "\r\n\r\n\r\n"
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
+# Command line argument help info 
+parser = argparse.ArgumentParser(description='This script listens on a port (defined in the config.json file) for syslog messages from Arbor Peakflow and when it sees a message that a DDoS attack has stopped, it will launch the query.py script which query Peakflow\'s API and retrieve the source IPs associated with the attack and then upload them to CableLab\'s CRITS database.')
+# Set argument of config.json file if specified
+parser.add_argument('-c','--config',help='Specify the path and filename of the config.json file.', required=False)
+# Parse the command line arguments
+args = parser.parse_args()
+
+
 #---- Import Config ----- 
 try:
-    pathConfigFile = appCurrentPath + '/config.json'
+    # If the config has been specified as a command line argument, then use this value as the path to the config file. 
+    # If not, then just use the existing directory that the app was started in.
+    if args.config is not None:
+       pathConfigFile = args.config
+    else:
+        pathConfigFile = appCurrentPath + '/config.json'
     print "Loading config: " + pathConfigFile
+    
     with open(pathConfigFile) as data_file:    
         configData = json.load(data_file)
     varSyslogPort = configData['local']['syslog_port']
@@ -69,16 +109,18 @@ except ValueError:
     print "Error! Could not load the config file: " + pathConfigFile + " \n\n" + sys.exc_info()[0]
     raise
 #-----------------------------------------------------------------------
-buf = 1500
-f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
-Local_IP = f.read()
+# broken in Docker
+#buf = 1500
+#f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+#Local_IP = f.read()
 
 addr = (Local_IP,varSyslogPort)
 if varSyslogProto == "udp":
     UDPSock = socket(AF_INET,SOCK_DGRAM) # Create socket and bind to address
     UDPSock.bind(addr)
 
-varAppendText = "\n------------------ DDoS Collector v" + appVersion + " @ " + Local_IP + " READY! (" + appTime + ") Listening on " + str(varSyslogProto) + " " + str(varSyslogPort) + " ------------------\n"
+#varAppendText = "\n------------------ DDoS Collector v" + appVersion + " @ " + Local_IP + " READY! (" + appTime + ") Listening on " + str(varSyslogProto) + " " + str(varSyslogPort) + " ------------------\n"
+varAppendText = "\n------------------ DDoS Collector v" + appVersion + " READY! (" + appTime + ") Listening on " + str(varSyslogProto) + " " + str(varSyslogPort) + " ------------------\n"
 print varAppendText
 
 if varlocal_log_to_file == True:
@@ -115,13 +157,15 @@ while 1:
             varAlertID = varAlertID[:10]  # max length 10 chars
             #------------ Start Child Process ----------------------------------
             if '.py' in varSourceCollector:
-                varCommand = "python " + appCurrentPath + "/" + varSourceCollector + " " + varAlertID
+                if pathConfigFile is not None:
+                    varCommand = "python " + appCurrentPath + "/" + varSourceCollector + " -c  " + pathConfigFile +  " -a " + varAlertID
+                    print "This is our varCommand:", varCommand
+                else:
+                    varCommand = "python " + appCurrentPath + "/" + varSourceCollector +  " " + varAlertID
+                    print "This is our varCommand:", varCommand
             else:
                 varCommand = "./" + varSourceCollector + " " + varAlertID
-                
+
             print "Spawning New Process -> ",varCommand
-#           p = subprocess.Popen("python " + appCurrentPath + "/getSources2.py " + varAlertID, bufsize=-1, shell=True, executable=None, stdin=None, stdout=None, stderr=None)
-#           p = subprocess.Popen(varCommand, bufsize=-1, shell=True, executable=None, stdin=None, stdout=None, stderr=None)
-            #subprocess.Popen(["rm","-r","some.file"])
             p = subprocess.Popen(varCommand, bufsize=-1, shell=True, executable=None, stdin=None, stdout=None, stderr=None)
 UDPSock.close()
--- getSources.py
+++ getSources.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # Copyright (c) 2016-2017 Christopher Gray & Daniel Phan - Comcast Cable Communications LLC.
+# Copyright (c) 2017 Rich Compton - Charter Communications
 # All rights reserved.
 # https://github.com/c2theg/DDoS_Infomation_Sharing
 
-#--- Force Python2 as Suds doesn't support Python3 fully as of 11/10/2016
+#--- Force Python2 as Suds 0.04 doesn't support Python3 fully as of 11/10/2016
 #This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com
 #Inital: 12/23/16  Updated: 2/5/17
 from socket import *
@@ -20,12 +21,6 @@ import urllib2
 # https://bitbucket.org/jurko/suds
 import suds
 import suds.client
-#------ MaxMind Legacy and GeoIP2 API's ------  
-try:
-    import GeoIP # https://github.com/maxmind/geoip-api-python
-    import geoip2.database  # GeoIP2 - https://github.com/maxmind/GeoIP2-python 
-except OSError:
-    pass
 #---- Custom Libraries ----
 # Add current dir to search path.
 #sys.path.insert(0, "libraries")
@@ -42,24 +37,51 @@ PythonVer = sys.version_info
 appCurrentPath = os.getcwd()
 appCurrentPath.replace("\/",'\\/')
 appTime = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-appVersion = '0.2.23'
+appVersion = '0.2.25'
 SourcesFound = 0
 OutputDict = []
 OutputJSON = ""
 SendData = []
-app_hostname = socket.getfqdn()
-print "Hostname: " + app_hostname
+varlocal_log_to_file = ''
+#app_hostname = socket.getfqdn()
+#print "Hostname: " + app_hostname
+#------ GET CLI Input -----------------------------------------------------------
+try:
+    parser = argparse.ArgumentParser(description='This script retrieves the source IPs for a given alert ID specified and uploads the source IPs to the CableLabs CRITS database.')
+    parser.add_argument('-c','--config', help='Specify the location of the config.json file.',required=False)
+    parser.add_argument('-a','--alert', help='Specify the Peakflow Alert ID.',required=True)
+    args = parser.parse_args()
+    pathConfigFile = args.config
+    varArgumentReceieved = args.alert
+    #varArgumentReceieved = int(varArgumentReceieved)
+    if varArgumentReceieved is None:
+        print "\n\nEnter a Alert ID to lookup \n\n"
+        sys.exit(0)
+except OSError:
+    pass
+
 #---- Import Config ----- 
 try:
-    ConfigFilePath = appCurrentPath + '/config.json'
-    #print "Loading Config file: ", ConfigFilePath
+    # If the config has been specified as a command line argument, then use this value as the path to the config file.  If not, then just use the existing directory that the app was started in.
+    if args.config is not None:
+       ConfigFilePath = args.config
+    else:
+        ConfigFilePath = appCurrentPath + '/config.json'
+    print "Loading Config file: ", ConfigFilePath
+    if varlocal_log_to_file == True:
+        varAppendText = str("\nLoading config file: " + ConfigFilePath + "\n")
+        with open(varLogFile, "a") as myfile:
+            myfile.write(varAppendText)
+            myfile.close
+                
     with open(ConfigFilePath) as data_file:
         configData = json.load(data_file)
         #----------------------------------------
     #print "Config Data: ", configData
-    varUpdate_auto = configData['local']['auto_updates']
-    varUpdate_url = configData['local']['updates_url']
-    varUpdate_checkEvery = configData['local']['update_checkEvery']
+    if 'auto_updates' in configData['local']:
+        varUpdate_auto = configData['local']['auto_updates']
+        varUpdate_url = configData['local']['updates_url']
+        varUpdate_checkEvery = configData['local']['update_checkEvery']
     #-----------------------------------------------------------------
     varArborURL = 'https://' + configData['arbor']['url']
     varArborPort = configData['arbor']['port']
@@ -88,36 +110,36 @@ try:
     varGeoIPDir = configData['geo']['files_path']
     varRemoteURL = configData['remote']
 
-
 except ValueError:
     print ("Oops! had a problem with the config file", sys.exc_info()[0])
     sys.exit(0)
-#------ GET CLI Input -----------------------------------------------------------
-try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("echo")
-    args = parser.parse_args()
-    varArgumentReceieved = args.echo[:10]
-    #varArgumentReceieved = int(varArgumentReceieved)
-    if varArgumentReceieved is None:
-        print "\n\nEnter a Alert ID to lookup \n\n"
-        sys.exit(0)
-except OSError:
-    pass
+    
+#------ MaxMind Legacy and GeoIP2 API's ------ 
+if varGeoIPDir: 
+    try:
+        import GeoIP # https://github.com/maxmind/geoip-api-python
+        import geoip2.database  # GeoIP2 - https://github.com/maxmind/GeoIP2-python 
+    except OSError:
+        pass
+else:
+    print "\r\n ***** Geolocation has been DISABLED! ***** \r\n"
 #-------------- Functions -----------------------------------------------------------------------------------------------
 def props(x):
     return dict((key, getattr(x, key)) for key in dir(x) if key not in dir(x.__class__))
 #------------------------------------------------------ Code -------------------------------------------------------------
 appHeader = """\
-     _____ _       _     _           _              _____    __    _____             
-    |     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
-    |   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
-    |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
-                                |_|                                             |___|
-                                      __     
-                                     / _|___ 
-                                     > _|_ _|
-                                     \_____| 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+                                           _   
+                                         _| |_ 
+                                        |   __|
+                                        |   __|
+                                        |_   _|
+                                          |_|                              
                                              
                       ___            _     _   ___ _              
                      |   \ __ _ _ _ (_)___| | | _ \ |_  __ _ _ _  
@@ -132,7 +154,20 @@ appHeader = """\
                       / __|___ _ __  _ __     | |  | |  / __|
                      | (__/ _ \ '  \| '  \ _  | |__| |_| (__ 
                       \___\___/_|_|_|_|_|_(_) |____|____\___|
-                                                             
+                                     
+-----------------------------------------------------------------------------------------
+                                                                  
+             _____ _     _      _____               _                         
+            | __  |_|___| |_   |     |___ _____ ___| |_ ___ ___               
+            |    -| |  _|   |  |   --| . |     | . |  _| . |   |              
+            |__|__|_|___|_|_|  |_____|___|_|_|_|  _|_| |___|_|_|              
+                                               |_|                            
+                                                                              
+                     _____ _           _              _____                   
+                    |     | |_ ___ ___| |_ ___ ___   |     |___ _____ _____   
+                    |   --|   | .'|  _|  _| -_|  _|  |   --| . |     |     |_ 
+                    |_____|_|_|__,|_| |_| |___|_|    |_____|___|_|_|_|_|_|_|_|
+                                                                                                       
                           ___ __  _  __         ___ __  _ ____ 
                          |_  )  \/ |/ /   ___  |_  )  \/ |__  |
                           / / () | / _ \ |___|  / / () | | / / 
@@ -161,16 +196,17 @@ else:
 
 page = ''
 #-------------- Get the GeoIP Database file ------------------------------------------------------------------------------
-print 'This product includes GeoLite2 data created by MaxMind, available from <a href="http://www.maxmind.com">http://www.maxmind.com</a>.'
-if varlocaldebugging == True:
-    print "Loading the following Maxmind GeoIP database from path: " + varGeoIPDir + ", GeoLite2-City.mmdb, GeoIPASNum.dat, GeoIPASNumv6.dat \n\n"    
-try:
-    geoIP2_City     = geoip2.database.Reader(varGeoIPDir + 'GeoLite2-City.mmdb')
-    geoipASN_v4     = GeoIP.open(varGeoIPDir + "GeoIPASNum.dat",    GeoIP.GEOIP_MEMORY_CACHE) #--- Needed as GeoIP2 doesn't offer ASN for free ---
-    geoipASN_v6     = GeoIP.open(varGeoIPDir + "GeoIPASNumv6.dat",  GeoIP.GEOIP_MEMORY_CACHE)
-except OSError:
-    print "Could not load GeoIP file(s)! Please run \n"
-    sys.exit(0)
+if varGeoIPDir:
+    print 'This product includes GeoLite2 data created by MaxMind, available from <a href="http://www.maxmind.com">http://www.maxmind.com</a>.'
+    if varlocaldebugging == True:
+        print "Loading the following Maxmind GeoIP database from path: " + varGeoIPDir + ", GeoLite2-City.mmdb, GeoIPASNum.dat, GeoIPASNumv6.dat \n\n"    
+    try:
+        geoIP2_City     = geoip2.database.Reader(varGeoIPDir + 'GeoLite2-City.mmdb')
+        geoipASN_v4     = GeoIP.open(varGeoIPDir + "GeoIPASNum.dat",    GeoIP.GEOIP_MEMORY_CACHE) #--- Needed as GeoIP2 doesn't offer ASN for free ---
+        geoipASN_v6     = GeoIP.open(varGeoIPDir + "GeoIPASNumv6.dat",  GeoIP.GEOIP_MEMORY_CACHE)
+    except OSError:
+        print "Could not load GeoIP file(s)! Please run \n"
+        sys.exit(0)
 #-------------------------------------------------------------------------------------------------------------------------    
 print "Waiting " + str(varlocal_wait_before_pull) + " seconds before pulling data from Arbor."
 time.sleep(varlocal_wait_before_pull)
@@ -178,7 +214,9 @@ time.sleep(varlocal_wait_before_pull)
 misuseTypes = ''
 try:
     alertDetailsResponseRAW = cls_http.HTTP_GET_rjson( str(varArborURL + "/arborws/alerts?api_key=" + varArborKey + "&filter=" + varArgumentReceieved) )
-
+    print "REST Received: "
+    print alertDetailsResponseRAW
+    print "\r\n -------------------------------------------- \r\n\r\n";
     for i in alertDetailsResponseRAW:
         misuseTypes += str(i['misuseTypes'])
     misuseTypes = misuseTypes[1:]  # [u'TCP SYN']
@@ -217,7 +255,7 @@ except ValueError:
 #--------------------------------------------------------------------------------------------------------
 try:
     ArborResultJSON = props(ArborResultRAW)
-    if varlocal_log_to_file == True:
+    if varlocal_log_to_file:
         varAppendText = str("\nAlert ID: " + varArgumentReceieved + "\n")
         with open(varLogFile, "a") as myfile:
             myfile.write(varAppendText)
@@ -240,54 +278,60 @@ try:
                         CleanIP = x.id.split("/")[0]                    
                         TempOutputDict['IPaddress'] = CleanIP
                         #---- do GeoIP Lookup ----------
-                        gir = geoIP2_City.city(CleanIP)
-                        if gir is not None:
-                            #------------ ASN ---------
-                            if ':' in x.id:  # this is a IPv6 Address
-                                girASN = geoipASN_v6.org_by_addr(CleanIP)
+                        if varGeoIPDir:
+                            print "Looking up Geo IP info... ";
+                            gir = geoIP2_City.city(CleanIP)
+                            if gir is not None:
+                                #------------ ASN ---------
+                                if ':' in x.id:  # this is a IPv6 Address
+                                    girASN = geoipASN_v6.org_by_addr(CleanIP)
+                                else:
+                                    girASN = geoipASN_v4.org_by_addr(CleanIP)
+                                #print "ASN: " + str(girASN)   # AS16509 Amazon.com, Inc.
+                                if girASN is not None:
+                                    try:
+                                        ASN_Number = girASN.split(' ', 1)[0] # AS36351
+                                        ASN_Number = ASN_Number[2:]
+                                        ASN_Name = girASN.split(' ', 1)[1]   # Amazon.com, Inc.
+                                        ASN_Name = re.sub('[ ](?=[ ])|[^-_,A-Za-z0-9. ]+', '', ASN_Name)
+                                    except ValueError:
+                                        raise
+                                else:
+                                    ASN_Number = 0
+                                    ASN_Name = "na"
+                                #---------- Generate Output ---------------------------------------------
+                                if gir.city.name is not None:
+                                    TempOutputDict['City'] = str(gir.city.name).decode('utf-8', 'ignore')
+                                else:
+                                    TempOutputDict['City'] = "na"
+                                    
+                                if gir.subdivisions.most_specific.iso_code is not None:
+                                    TempOutputDict['State'] = str(gir.subdivisions.most_specific.iso_code).decode("utf-8", 'ignore')
+                                else:
+                                    TempOutputDict['State'] = "na"
+    
+                                if gir.country.iso_code is not None:
+                                    TempOutputDict['Country'] = str(gir.country.iso_code).decode("utf-8", 'ignore') # 'US'
+                                else:
+                                    TempOutputDict['Country'] = "na"
+    
+                                #TempOutputDict['latitude'] = gir.location.latitude
+                                #TempOutputDict['longitude'] = gir.location.longitude
+                                TempOutputDict['Extra'] = str(gir.location.latitude) + "," + str(gir.location.longitude)
+                                TempOutputDict['SourceASN'] = ASN_Number
+                                TempOutputDict['SourceASNName'] = ASN_Name                                 
+                                #print "Work on: " + TempOutputDict
                             else:
-                                girASN = geoipASN_v4.org_by_addr(CleanIP)
-                            #print "ASN: " + str(girASN)   # AS16509 Amazon.com, Inc.
-                            if girASN is not None:
-                                try:
-                                    ASN_Number = girASN.split(' ', 1)[0] # AS36351
-                                    ASN_Number = ASN_Number[2:]
-                                    ASN_Name = girASN.split(' ', 1)[1]   # Amazon.com, Inc.
-                                    ASN_Name = re.sub('[ ](?=[ ])|[^-_,A-Za-z0-9. ]+', '', ASN_Name)
-                                except ValueError:
-                                    raise
-                            else:
-                                ASN_Number = 0
-                                ASN_Name = "na"
-                            #---------- Generate Output ---------------------------------------------
-                            if gir.city.name is not None:
-                                TempOutputDict['City'] = str(gir.city.name).decode('utf-8', 'ignore')
-                            else:
-                                TempOutputDict['City'] = "na"
-                                
-                            if gir.subdivisions.most_specific.iso_code is not None:
-                                TempOutputDict['State'] = str(gir.subdivisions.most_specific.iso_code).decode("utf-8", 'ignore')
-                            else:
-                                TempOutputDict['State'] = "na"
-
-                            if gir.country.iso_code is not None:
-                                TempOutputDict['Country'] = str(gir.country.iso_code).decode("utf-8", 'ignore') # 'US'
-                            else:
-                                TempOutputDict['Country'] = "na"
-
-                            #TempOutputDict['latitude'] = gir.location.latitude
-                            #TempOutputDict['longitude'] = gir.location.longitude
-                            TempOutputDict['Extra'] = str(gir.location.latitude) + "," + str(gir.location.longitude)
-                            TempOutputDict['TotalBPS'] = x.net.bps
-                            TempOutputDict['TotalPPS'] = x.net.pps
-                            TempOutputDict['SourceASN'] = ASN_Number
-                            TempOutputDict['SourceASNName'] = ASN_Name 
-                            TempOutputDict['AttackType'] = misuseTypes
-                            #print "Work on: " + TempOutputDict
-                            OutputDict.append(TempOutputDict)
+                                if varlocaldebugging == True:
+                                    print "GeoIP came back empty for IP: " + CleanIP
                         else:
-                            if varlocaldebugging == True:
-                                print "GeoIP came back empty for IP: " + CleanIP
+                            print "NOT looking up Geo IP info... ";
+                                    
+                        TempOutputDict['TotalBPS'] = x.net.bps
+                        TempOutputDict['TotalPPS'] = x.net.pps
+                        TempOutputDict['AttackType'] = misuseTypes
+                        
+                        OutputDict.append(TempOutputDict)
                     #--- Clear temp variables ---
                     TempOutputDict = None
                     gir = None
@@ -295,7 +339,8 @@ try:
             else:
                 print "Ignoring CIDR: " + x.id
 
-        geoIP2_City.close()
+        if varGeoIPDir:
+            geoIP2_City.close()
 
         if varlocaldebugging == True:
             print "\n\n Finished generating JSON payload, which containes ", SourcesFound, " DDoS sources! \n\n"
