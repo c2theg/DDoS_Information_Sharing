585eb82143260386d882d17d5ae260d2b2de169c
Chris Gray <christophermjgray@gmail.com>
2017-06-24 10:55:22 -0400

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -18,8 +18,8 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.5.3             \r\n
-Last Updated:  6/12/2017
+Version:  0.5.4             \r\n
+Last Updated:  6/24/2017
 \r\n \r\n
 Updating system first..."
 sudo apt-get update 
@@ -29,12 +29,13 @@ wait
 #--------------------------------------------------------------------------------------------
 if [ -s "getSources.py" ] 
 then
-    echo "Deleting Old files!!!"
+    echo "Deleting old files!!!"
     rm updater.sh
     rm collector.py
     rm getSources.py
     rm func_REST.py
-    
+    rm client.html
+    rm client_submissions.html
     rm config.json.new
 fi
 
@@ -43,21 +44,22 @@ echo "\n\n\n\n"
 echo "Downloading latest versions to all files ... \n\n\n\n"
 
 sudo wget -O updater.sh "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/updater.sh"
+sudo wget -O func_REST.py "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/func_REST.py"
+sudo wget -O func_common.py "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/func_common.py"
 sudo wget -O collector.py "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/collector.py"
 sudo wget -O getSources.py "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/getSources.py"
 sudo wget -O config.json.new "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/config.json"
-sudo wget -O func_REST.py "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/func_REST.py"
+
+sudo wget -0 client.html "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/client.html"
+sudo wget -0 client_submissions.html "https://raw.githubusercontent.com/c2theg/DDoS_Information_Sharing/master/client_submissions.html"
 
 wait
 echo "\n\n\n Done... \n\n"
 #----- Update priv. -----------
-
 chmod u+x updater.sh
 chmod u+x collector.py
 chmod u+x getSources.py
 chmod u+x func_REST.py
-
 #------------------------------
 echo "Done! \r\n \r\n"
-
-python2.7 collector.py -c config.json
+python2 ./collector.py -c config.json
