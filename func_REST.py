48bc8d021461585a152e3678bfb8b25ee6b85106
Chris Gray <christophermjgray@gmail.com>
2019-05-16 16:19:33 -0400

    Update func_REST.py

--- func_REST.py
+++ func_REST.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016-2017 Christopher Gray & Daniel Phan - Comcast Cable Communications LLC.
+# Copyright (c) 2016-2017 Christopher Gray & Daniel Phan
 # All rights reserved.
 # https://github.com/c2theg/DDoS_Infomation_Sharing
 import errno, sys, json, os, time, logging, argparse, base64, ssl, re, string
